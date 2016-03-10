dd8f5df5c8b650deed69758363287f58c9842b6c
Jason Wu <jasonwupilly1997@gmail.com>
2016-03-09 21:52:01 -0500

    add audioservice singleton

--- app/src/main/java/edu/gatech/wavevis/app/AudioService.java
+++ app/src/main/java/edu/gatech/wavevis/app/AudioService.java
@@ -1,7 +1,16 @@
 package edu.gatech.wavevis.app;
 
+import be.tarsos.dsp.AudioDispatcher;
+
 public class AudioService {
     private static AudioService ourInstance;
+
+    private final int SAMPLE_RATE = 8000;
+    private final int BUFFER_SIZE = 1024;
+
+    private AudioDispatcher dispatcher;
+
+    private Thread dispatcherThread;
 
     public static AudioService getInstance() {
         if (ourInstance == null) {
@@ -10,6 +19,9 @@ public class AudioService {
         return ourInstance;
     }
 
+
+
     private AudioService() {
+
     }
 }
